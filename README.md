6e2ea25dcd65738b1225bd86fb37fc9a7d248241
Stephen Riley <jstephen.riley@yahoo.com>
2025-12-30 16:24:16 -0800

    initial check-in

---
+++ README.md
@@ -0,0 +1,17 @@
+# MIL-STD-2525 training dataset generator
+
+This repository contains a dataset generator for MIL-STD-2525 training data.  It uses [milsymbol-py](https://github.com/stephen-riley/milsymbol-py) to generate PNGs of common MIL-STD-2525 symbols and a LLaVA JSON file for training Gemma 3 to identify the symbols. 
+
+## Getting started
+
+This repo uses `uv` for package management.  You must have `uv` installed to use this repo.
+
+1. Clone [milsymbol-py](https://github.com/stephen-riley/milsymbol-py)
+1. Clone this repo
+1. `cd milstd2525-training-data`
+1. `uv venv`
+1. `source .venv/bin/activate`
+1. `uv sync`
+1. `uv run gen_symbols.py`
+
+This will generate `output/train.json` and `output/symbols/*png`.
---
+++ gen_symbols.py
@@ -0,0 +1,80 @@
+import csv
+import json
+import milsymbolpy as ms
+import os
+import re
+
+SYMBOL_SIZE = 150
+catalog = []
+
+os.makedirs("output/symbols", exist_ok=True)
+
+
+def gen_symbol(affiliation, id, echelon):
+    affil_hr = "Friendly" if affiliation == "FR" else "Enemy"
+    affil_code = "03" if affiliation == "FR" else "06"
+
+    if len(id) == 12:
+        sidc = f"13{affil_code}1000{echelon}{id}00000000"
+    else:
+        sidc = f"13{affil_code}1000{echelon}{id}00000000000000"
+
+    print(sidc)
+
+    if len(sidc) != 30:
+        msg = f"Invalid SIDC: {sidc} (length is {len(sidc)})"
+        raise ValueError(msg)
+
+    s = ms.Symbol(sidc, {"size": SYMBOL_SIZE})
+    desc = s.get_desc()
+    desc = re.sub(r"\[..\] ", "", desc)
+    png_fname = f"output/symbols/{sidc}.png"
+    s.as_png(png_fname, training=True)
+
+    catalog.append(
+        {
+            "id": sidc,
+            "image": png_fname,
+            "conversations": [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "image"},
+                        {
+                            "type": "text",
+                            "text": "Identify the unit depicted in this MIL-STD-2525 symbol.",
+                        },
+                    ],
+                },
+                {
+                    "role": "assistant",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": f"{affil_hr} {desc}",
+                        }
+                    ],
+                },
+            ],
+        }
+    )
+
+
+for affiliation in ["FR", "EN"]:
+    with open("symbol_catalog.tsv", "r", newline="", encoding="utf-8") as tsvfile:
+        # Set the delimiter to a tab character
+        tsvreader = csv.reader(tsvfile, delimiter="\t")
+        next(tsvreader)  # skip header line
+
+        for row in tsvreader:
+            if row[1][-4:] == "0000":
+                continue
+            if row[2] == "x":
+                (start, end) = row[3].split("-")
+                for i in range(int(start), int(end) + 1):
+                    gen_symbol(affiliation, row[1], f"1{i}")
+            else:
+                gen_symbol(affiliation, row[1], "00")
+
+with open("output/train.json", "w", encoding="utf-8") as catalog_file:
+    json.dump(catalog, catalog_file, indent=2, ensure_ascii=False)
